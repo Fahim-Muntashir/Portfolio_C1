412e94c358aaff1b4068a1885eec8a5015eecfe0
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-02-15 13:58:18 +0600

    Masive Growth added

--- components/Home/WhereIHaveWorked/Descriptions/MassiveGrowth.tsx
+++ components/Home/WhereIHaveWorked/Descriptions/MassiveGrowth.tsx
@@ -5,17 +5,22 @@ import { getTasksTextWithHighlightedKeyword } from "./taskAndType";
 export default function MassiveGrowth() {
     const tasks = [
         {
-            text: "Spearhead & implemented a new design user workflow system for the Frond-End Architecture of a NFT Marketplace.",
-            keywords: ["NFT Marketplace"],
+            text: "As CEO and Founder of Massive Growth, my role as Chief Web Developer empowers our team to drive innovation and foster growth for clients. Drawing on deep expertise, we create exceptional digital solutions.",
+            keywords: ["exceptional", "Massive Growth", "our team"],
         },
         {
-            text: "Worked with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Next.js/React, AWS and Vercel.",
-            keywords: ["Next.js/React", "AWS", "Vercel"],
+            text: "At Massive Growth, I lead the development of digital solutions, focusing on driving innovation and growth. My role as Chief Web Developer leverages our expertise to deliver exceptional results for our clients.",
+            keywords: ["Next.js", "clients", "Massive", "results"],
         },
         {
-            text: "Interfaced with developers on a daily basis, providing technological expertise.",
-            keywords: [],
-        },
+            text: "As Chief Web Developer, my work centers on developing cutting-edge digital solutions that drive growth and innovation. I lead a talented team, and our goal is to create lasting impact.",
+            keywords: ["impact", "digital solutions",
+                "growth", "innovation"
+            ],
+        }, {
+            text: "My role at Massive Growth as Chief Web Developer involves spearheading the development of digital solutions. My expertise ensures our team delivers results that drive innovation and growth for our clients.",
+            keywords: ["clients", "spearheading", "digital solutions"],
+        }
     ];
 
     return (
@@ -24,15 +29,15 @@ export default function MassiveGrowth() {
                 <div className="flex flex-col spacey-y-2">
                     {/* Title */}
                     <span className="text-gray-100 sm:text-lg text-sm font-Arimo tracking-wide">
-                        Software Engineer <span className="text-AAsecondary">@ Blockchain</span>
+                        Founder <span className="text-AAsecondary">@MassiveGrowth</span>
                     </span>
                     {/* Date */}
-                    <span className="font-mono text-xs text-gray-500">Mars - August 2022</span>
+                    <span className="font-mono text-xs text-gray-500">Sep 2023 - Running </span>
                     <span className="font-mono text-xs text-AAsecondary hover:cursor-pointer" style={{ fontSize: "0.6rem" }}
                         // set on click to open the website
-                        onClick={() => window.open("https://www.trouvetavoie.io/", "_blank")}
+                        onClick={() => window.open("https://massivegrowth.net/", "_blank")}
                     >
-                        www.trouvetavoie.io
+                        www.massivegrowth.net
                     </span>
                 </div>
                 <div className="flex flex-col space-y-4 sm:text-sm text-xs">
